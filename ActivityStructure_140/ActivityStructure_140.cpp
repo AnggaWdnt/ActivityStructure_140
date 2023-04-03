1565cd51d8ae80545adc9b1d2026db50716770c9
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-04-03 10:55:37 +0700

    penambahan cout dan cin

--- ActivityStructure_140/ActivityStructure_140.cpp
+++ ActivityStructure_140/ActivityStructure_140.cpp
@@ -19,10 +19,22 @@ int main()
     mhs1.alamat = "Wonosobo";
     mhs1.umur = 20;
 
-    
+    cout << "masukan NIM :";
+    cin >> mhs2.NIM;
+    cout << "Masukan Nama :";
+    cin >> mhs2.nama;
+    cout << "Masukan Alamat :";
+    cin >> mhs2.alamat;
+    cout << "Masukan Umur :";
+    cin >> mhs2.umur;
 
-}
-int main()
-{
-    std::cout << "Hello World!\n";
+    cout << "\nim = " << mhs1.nim;
+    cout << "\nNama = " << mhs1.nama;
+    cout << "\nAlamat = " << mhs1.alamat;
+    cout << "\nUmur = " << mhs1.umur;
+
+    cout << "\n\nnim = " << mhs2.nim;
+    cout << "\nNama = " << mhs2.nama;
+    cout << "\nAlamat = " << mhs2.alamat;
+    cout << "\nUmur = " << mhs2.umur;
 }
